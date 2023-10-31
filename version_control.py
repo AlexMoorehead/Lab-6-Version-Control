db271600f22a63e04d3271281d14ffdd1a25e11f
kingofnerds1453 <149018767+kingofnerds1453@users.noreply.github.com>
2023-10-31 14:20:53 -0400

    my contribution to Alex's code

--- version_control.py
+++ version_control.py
@@ -18,8 +18,27 @@ def encode(password):
 
 # Takes in encoded user password and shifts each digit down by 3 numbers.
 def decode(password):
-    pass
-
+    usable_list = []
+    for element in password:
+        item = int(element) - 3
+        usable_list.append(item)
+        # decided to modify Alex's logic since it seemed more conscise then mine
+        n = 0
+        for item in usable_list:
+           if item == -1:
+               usable_list[n] = 9
+           if item == -2:
+               usable_list[n] = 8
+           # deals with the negative numbers
+           if item == -3:
+               usable_list[n] = 7
+           n += 1
+    n = 0
+    for element in usable_list:
+        usable_list[n] = str(usable_list[n])
+        n += 1
+    decoded = "".join(usable_list)
+    return decoded
 
 # Main program loop, takes menu inputs and calls respective methods.
 while True:
@@ -32,7 +51,10 @@ while True:
         print("Your password has been encoded and stored!\n")
 
     elif menu_input == 2:
-        print('')
+        encode = encode(orig_pass)
+        decoded = decode(encode)
+       # uses the actual programs that are written
+        print(f"The encoded password is {encode}, and the original password is {decoded}.")
         pass
 
     elif menu_input == 3:
